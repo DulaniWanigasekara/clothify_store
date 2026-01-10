0bdc2edcf112d84b2c7aaecccb20a123c940e2aa
DulaniWanigasekara <dulani.piusha2003@gmail.com>
2026-01-10 22:46:36 +0530

    Starter class completed

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,4 +1,13 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Starter {
-
+public class Starter extends Application {
+    public static void main(String[] args) {launch();}
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashBoardForm.fxml"))));
+        stage.show();
+    }
 }
