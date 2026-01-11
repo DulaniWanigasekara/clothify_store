1902e6e58f610794ee395120bc215c3b2abc64bf
DulaniWanigasekara <dulani.piusha2003@gmail.com>
2026-01-11 13:11:42 +0530

    Report dto class completed

--- src/main/java/model/dto/Report.java
+++ src/main/java/model/dto/Report.java
@@ -1,4 +1,14 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Report {
+    public String orderId;
+    public String customerId;
+    public String name;
 }
