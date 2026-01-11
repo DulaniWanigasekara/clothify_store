63abc219f2625979a555c62914f991e5b1b7862e
DulaniWanigasekara <dulani.piusha2003@gmail.com>
2026-01-11 12:42:59 +0530

    Employee dto class completed

--- src/main/java/model/dto/Employee.java
+++ src/main/java/model/dto/Employee.java
@@ -1,4 +1,21 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Employee {
+    private String id;
+    private String name;
+    private String nic;
+    private String dob;
+    private String position;
+    private double salary;
+    private String contactNumber;
+    private String address;
+    private String joinedDate;
+    private String status;
 }
