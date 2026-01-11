8f25764644382950ecd11433e19b782b9e2ae641
DulaniWanigasekara <dulani.piusha2003@gmail.com>
2026-01-11 13:03:06 +0530

    Item dto class completed

--- src/main/java/model/dto/Item.java
+++ src/main/java/model/dto/Item.java
@@ -1,4 +1,24 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Item {
+    private String id;
+    private String name;
+    private String category;
+    private String size;
+    private double price;
+    private int qty;
+    private boolean available;
+    private double total;
+
+    public boolean getAvailable() {
+        return this.available;
+    }
+
 }
