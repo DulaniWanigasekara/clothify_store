1d986100f1bd6e748768293d689ad0e4cadfd780
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-12 22:15:43 +0530

    database connection created in DBConnection class

--- src/main/java/dbConnection/DBConnection.java
+++ src/main/java/dbConnection/DBConnection.java
@@ -1,4 +1,14 @@
 package dbConnection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static DBConnection instance;
+    private Connection connection;
+
+    public DBConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clothifydb", "root", "1234");
+    }
 }
