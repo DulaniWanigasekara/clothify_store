186bc30f3136fe27d848c43ea2e4a3306062a773
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-18 13:29:22 +0530

    getAllItems method in DashboardRepository created

--- src/main/java/repository/DashBoardRepository.java
+++ src/main/java/repository/DashBoardRepository.java
@@ -1,12 +1,21 @@
 package repository;
 
+import dbConnection.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.dto.Customer;
 import model.dto.Item;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DashBoardRepository {
 
     ObservableList<Item> items = FXCollections.observableArrayList();
     ObservableList<Customer> customers = FXCollections.observableArrayList();
+
+    public ObservableList<Item> getAllItems() {
+        return items;
+    }
 }
