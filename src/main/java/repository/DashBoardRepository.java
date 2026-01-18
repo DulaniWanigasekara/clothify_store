eaa4169954064bff9258695d96f4021a740436f9
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-18 13:17:42 +0530

    Observablelist created in DashBoardRepository class

--- src/main/java/repository/DashBoardRepository.java
+++ src/main/java/repository/DashBoardRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.dto.Customer;
+import model.dto.Item;
+
 public class DashBoardRepository {
+
+    ObservableList<Item> items = FXCollections.observableArrayList();
+    ObservableList<Customer> customers = FXCollections.observableArrayList();
 }
